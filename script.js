2414d23b2f82f4db7008fbc679fb3d2d96708e2f
Mority <jo.mority@gmail.com>
2014-05-07 10:10:42 +0200

    g

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ var container;
 var renderer;
 var scene;
 var camera;
+var player;
+var ambientLight;
 
 function onLoad() {
-
+    //initialize
     container = document.getElementById("container");
     container.style.width = window.innerWidth + "px";
     container.style.height = window.innerHeight + "px";
@@ -20,6 +22,20 @@ function onLoad() {
 
     scene = new THREE.Scene();
 
+    camera = new THREE.PerspectiveCamera(45, container.offsetWidth/container.offsetHeight, 1, 1000);
+    camera.position.y = 5;
+    camera.position.z = 5;
+    camera.rotation.x = -Math.PI/4;
+    scene.add(camera);
+
+
+    //create player
+    player = new THREE.Mesh(new THREE.SphereGeometry(1,20,10), new THREE.MeshPhongMaterial({color: 0xff0000, wireframe:true}));
+    scene.add(player);
+
+    //create ambient light
+    ambientLight = new THREE.AmbientLight(0x404040);
+    scene.add(ambientLight);
 
     addMouseHandler();
     run();
