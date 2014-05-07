cdd9c17158ae6f64c71b2096d0e2804fa7502fc8
amazingdev <ano@vollbio.de>
2014-05-07 10:22:56 +0200

    tim

--- levelgen.js
+++ levelgen.js
@@ -2,3 +2,8 @@
  * Created by gast-c11 on 07.05.14.
  */
 
+function floor() {
+    plane = new THREE.Mesh(new THREE.PlaneGeometry(1,20,10), new THREE.MeshPhongMaterial({color: 0xff0000, wireframe:false}));
+    scene.add(plane);
+}
+
