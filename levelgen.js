fc68eb9e0dc064f63ea3ccd34aecf336635c5f3d
amazingdev <ano@vollbio.de>
2014-05-07 10:59:56 +0200

    tim

--- levelgen.js
+++ levelgen.js
@@ -8,12 +8,22 @@ function floor() {
     plane.rotation.x = - Math.PI/2;
     plane.position.z = - vector.y/2.2;
     scene.add(plane);
-    var vector = new THREE.Vector2(10,100);
-    plane2 = new THREE.Mesh(new THREE.PlaneGeometry(vector.x,vector.y), new THREE.MeshPhongMaterial({color: 0xB452CD, wireframe:false}));
-    plane2.rotation.x = - Math.PI/2;
-    plane2.rotation.z = - Math.PI/2;
-    plane2.position.z = - vector.y/2.2;
-    scene.add(plane2);
+
+    var vector2 = new THREE.Vector2(10,100);
+    wall = new THREE.Mesh(new THREE.PlaneGeometry(vector2.x,vector2.y), new THREE.MeshPhongMaterial({color: 0xffffff, wireframe:false}));
+    wall.rotation.z = - Math.PI/2;
+    wall.rotation.y = Math.PI/2;
+    wall.position.z = - vector.y/2.2;
+    wall.position.x = - vector.x/2;
+    scene.add(wall);
+
+    var vector2 = new THREE.Vector2(10,100);
+    wall2 = new THREE.Mesh(new THREE.PlaneGeometry(vector2.x,vector2.y), new THREE.MeshPhongMaterial({color: 0xffffff, wireframe:false}));
+    wall2.rotation.z = - Math.PI/2;
+    wall2.rotation.y = - Math.PI/2;
+    wall2.position.z = - vector.y/2.2;
+    wall2.position.x = vector.x/2;
+    scene.add(wall2);
 
 }
 
