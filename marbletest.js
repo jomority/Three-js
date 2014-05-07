3891612a2ec2bf8d21b4bb581f0e10b914f619a5
amazingdev <ano@vollbio.de>
2014-05-07 10:00:57 +0200

    tim

--- marbletest.js
+++ marbletest.js
@@ -6,3 +6,29 @@ var renderer;
 var scene;
 var camera;
 var plane, plane2, plane3, plane4;
+
+function onLoad() {
+
+    container = document.getElementById("container");
+    container.style.width = window.innerWidth + "px";
+    container.style.height = window.innerHeight + "px";
+
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(container.offsetWidth, container.offsetHeight);
+
+    container.appendChild(renderer.domElement);
+
+    scene = new THREE.Scene();
+
+
+    camera = new THREE.PerspectiveCamera(45, container.offsetWidth/container.offsetHeight, 1, 100000);
+    camera.position.set(0,3,10);
+    camera.rotation.x = (-Math.PI/10);
+    scene.add(camera);
+
+    light = new THREE.DirectionalLight(0xffffff,1);
+    light.position.set(0,1,0);
+    scene.add(light);
+
+    light2 = new THREE.AmbientLight(0xaaaaaa);
+    scene.add(light2);
